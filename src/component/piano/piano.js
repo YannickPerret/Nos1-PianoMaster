8fd055048cdeee0a16b3a91102fe2725dd56fff0
PERRET Yannick <Yannick.PERRET@cpnv.ch>
2022-11-18 13:12:41 +0100

    update

--- src/component/piano/piano.js
+++ src/component/piano/piano.js
@@ -1,85 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Piano } from '../../helpers/piano';
 
 const PianoMaster = (props) => {
     const [audioActivated, setAudioActivated] = useState(true);
+    const pianoRef = useRef();
+    const [notation, setNotation] = useState([
+        ["white e", "c/4"],
+        ["black ds", "c#/e"],
+        ["white d", "d/4"],
+        ["black cs", "d#/4"],
+        ["white c", "e/4"],
+        ["white b", "f/4"],
+        ["black as", "f#/4"],
+        ["white a", "g/4"],
+        ["black gs", "g#/4"],
+        ["white g", "a/4"],
+        ["black fs", "a#/4"],
+        ["white f", "b/4"]
+    ])
+    let fragment=[];
 
-
-
-    const setParentKeyDown = (_key) => {
-        if(_key){
-
-            playMusic(_key)
-            return props.onWrite(_key)
+    useEffect(() => {
+        for (let i = 0; i < window.innerWidth; i += 755) {
+            notation.map(element => {
+                fragment += <li className={element[0]} onClick={(e => [Piano.play(element[1], audioActivated), props.onWrite(element[1])])}></li>
+            })
         }
-        return null
-    }
-    
-    // this function will play audio
-    const playMusic = (_key) =>{
-        const audio=document.getElementById(_key);
-        console.log(audio)
-        if(audio && audioActivated){
-            audio.currentTime=0;
-            audio.play();
-        }
-    }
+        
+        console.log(fragment)
+       
+    }, [])
+
 
     return (
         <>
             <label>Son du piano</label> <input type="checkbox" checked={audioActivated} onChange={(e => setAudioActivated(e.target.checked))} />
-                <ul className="set">
+            <ul className="set" id="piano">
                 {// c = do, d = RÉ, e = MI, f = FA, g = SOL, a = LA , b = SI
-                } 
-                <li className="white e" onClick={(e => [Piano.play("c/4", audioActivated), props.onWrite("c/4")])}></li>
-                <li className="black ds" onClick={(e => setParentKeyDown("c#/4"))}></li>
-                <li className="white d" onClick={(e => setParentKeyDown("d/4"))}></li>
-                <li className="black cs" onClick={(e => setParentKeyDown("d#/4"))}></li>
-                <li className="white c" onClick={(e => setParentKeyDown("e/4"))}></li>
-
-                <li className="white b" onClick={(e => setParentKeyDown("f/4"))}></li>
-                <li className="black as" onClick={(e => setParentKeyDown("f#/4"))}></li>
-                <li className="white a" onClick={(e => setParentKeyDown("g/4"))}></li>
-                <li className="black gs" onClick={(e => setParentKeyDown("g#/4"))}></li>
-                <li className="white g" onClick={(e => setParentKeyDown("a/4"))}></li>
-                <li className="black fs" onClick={(e => setParentKeyDown("a#/4"))}></li>
-                <li className="white f" onClick={(e => setParentKeyDown("b/4"))}></li>
-
-                {window.innerWidth > 390 ?
-                    <>
-                        <li className="white e" onClick={(e => setParentKeyDown("c/5"))}></li>
-                        <li className="black ds" onClick={(e => setParentKeyDown("c#/5"))}></li>
-                        <li className="white d" onClick={(e => setParentKeyDown("d/5"))}></li>
-                        <li className="black cs" onClick={(e => setParentKeyDown("d#/5"))}></li>
-                        <li className="white c" onClick={(e => setParentKeyDown("e/5"))}></li>
-
-                        <li className="white b" onClick={(e => setParentKeyDown("f/5"))}></li>
-                        <li className="black as" onClick={(e => setParentKeyDown("f#/5"))}></li>
-                        <li className="white a" onClick={(e => setParentKeyDown("g/5"))}></li>
-                        <li className="black gs" onClick={(e => setParentKeyDown("g#/5"))}></li>
-                        <li className="white g" onClick={(e => setParentKeyDown("a/5"))}></li>
-                        <li className="black fs" onClick={(e => setParentKeyDown("a#/5"))}></li>
-                        <li className="white f" onClick={(e => setParentKeyDown("b/5"))}></li>
-                    </>
-
-                :null}
+            }
             </ul>
 
-                
-                <audio id="c/4" src="../sounds/piano/c4.mp3"></audio>
-                <audio id="c#/4" src="../sounds/piano/c4D.mp3"></audio>
-                <audio id="d/4" src="../sounds/piano/d4.mp3"></audio>
-                <audio id="d#/4" src="../sounds/piano/d4D.mp3"></audio>
-                <audio id="e/4" src="../sounds/piano/e4.mp3"></audio>
-                <audio id="f/4" src="../sounds/piano/f4.mp3"></audio>
-                <audio id="f#/4" src="../sounds/piano/f4D.mp3"></audio>
-                <audio id="g/4" src="../sounds/piano/g4.mp3"></audio>
-                <audio id="g#/4" src="../sounds/piano/g4D.mp3"></audio>
-                <audio id="a/4" src="../sounds/piano/a4.mp3"></audio>
-                <audio id="a#/4" src="../sounds/piano/a4D.mp3"></audio>
-                <audio id="b/4" src="../sounds/piano/b4.mp3"></audio>
+            <audio id="c/4" src="../sounds/piano/c4.mp3"></audio>
+            <audio id="c#/4" src="../sounds/piano/c4D.mp3"></audio>
+            <audio id="d/4" src="../sounds/piano/d4.mp3"></audio>
+            <audio id="d#/4" src="../sounds/piano/d4D.mp3"></audio>
+            <audio id="e/4" src="../sounds/piano/e4.mp3"></audio>
+            <audio id="f/4" src="../sounds/piano/f4.mp3"></audio>
+            <audio id="f#/4" src="../sounds/piano/f4D.mp3"></audio>
+            <audio id="g/4" src="../sounds/piano/g4.mp3"></audio>
+            <audio id="g#/4" src="../sounds/piano/g4D.mp3"></audio>
+            <audio id="a/4" src="../sounds/piano/a4.mp3"></audio>
+            <audio id="a#/4" src="../sounds/piano/a4D.mp3"></audio>
+            <audio id="b/4" src="../sounds/piano/b4.mp3"></audio>
         </>
- 
     );
 };
 
